0f6bb39fba150ab9ffdb37b4e0fd339c5068b4e9
admin <admin@DESKTOP-7E5APTE>
2022-01-24 20:37:32 +0530

    user1 first commit

---
+++ src/test/java/com/crm/git/test/Demogittest.java
@@ -0,0 +1,16 @@
+package com.crm.git.test;
+
+import org.testng.annotations.Test;
+
+public class Demogittest {
+	
+	
+	@Test
+	public void demo()
+	{
+		System.out.println("hi user1, first commit ");
+		
+		
+	}
+
+}
